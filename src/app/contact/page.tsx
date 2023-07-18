d7d8ae496894d063a6e6ad47ed01b8497b3bd17d
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2023-07-17 21:30:01 -0300

    chore: inicial setup

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,26 @@
+export default function ContactPage () {
+
+    return (
+        <>
+            <h1>Contato</h1>
+            <ul>
+                <li>
+                    Rua Dr. Tuany Toledo Júnior, 20, Nova Pouso Alegre, CEP 37553-476, Pouso Alegre, MG
+                </li>
+                <li>
+                    (35) 3422-1540 (Mecânica)
+                </li>
+                <li>
+                    (35) 99105-4663 (Whatsapp)
+                </li>
+                <li>
+                    (35) 3025-0291 (Auto Peças)
+                </li>
+                <li>
+                    e-mail: mecmundial@bol.com.br
+                </li>
+                    
+            </ul>
+        </>
+    )
+}
