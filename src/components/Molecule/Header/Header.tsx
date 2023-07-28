a36b36269b216082632d86f5fedd3252ab690243
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2023-07-27 23:08:51 -0300

    feat: create header component and add it to root layout

--- src/components/Molecule/Header/Header.tsx
+++ src/components/Molecule/Header/Header.tsx
@@ -1,7 +1,22 @@
-
+import { Link } from "../Link";
 
 export default function Header() {
+
   return (
-    <div>Header</div>
+    <header
+      className="flex justify-center items-center w-full h-20 bg-zinc-700 "
+    >
+      <nav className="h-fit flex gap-2">
+        <Link path="/">
+          HOME
+        </Link>
+        <Link path="/about" >
+          A EMPRESA
+        </Link>
+        <Link path="/work-with-us" >
+          TRABALHE CONOSCO
+        </Link>
+      </nav>
+    </header>
   )
 }
---
+++ src/components/Molecule/Link/index.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import NextLink from "next/link";
+import { ReactNode } from "react";
+import { usePathname } from "next/navigation";
+
+
+interface LinkProps {
+    path: string
+    children: ReactNode
+}
+
+export function Link ({path, children}: LinkProps) {
+    const pathName = usePathname();
+
+    return (
+        <NextLink 
+            href={path}
+            className={
+                pathName === path 
+                ? "text-white py-1 px-4 rounded-full text-sm bg-blue-500" 
+                : "text-white py-1 px-4 rounded-full text-sm"
+            }
+        >
+            {children}
+        </NextLink>
+    )
+}
