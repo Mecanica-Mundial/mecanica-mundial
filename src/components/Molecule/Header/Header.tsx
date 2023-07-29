32d4877ed81ead3451865875696f5cd0e72e0431
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2023-07-28 21:26:26 -0300

    chore: add logo, shadow and icons to header

--- src/components/Molecule/Header/Header.tsx
+++ src/components/Molecule/Header/Header.tsx
@@ -1,11 +1,15 @@
+import Image from "next/image";
 import { LinkNav } from "../LinkNav";
+
+
 
 export default function Header() {
 
   return (
     <header
-      className="flex justify-center items-center w-full h-20 bg-zinc-700 "
+      className="flex justify-around items-center w-full h-20 bg-zinc-700 shadow-amber-200 shadow-lg"
     >
+      <Image width={218} height={50} src={"/images/mundial.svg"} alt="" className="w-56"/>
       <nav className="h-fit flex gap-2">
         <LinkNav path="/">
           HOME
@@ -17,6 +21,10 @@ export default function Header() {
           TRABALHE CONOSCO
         </LinkNav>
       </nav>
+      <div className="flex gap-4">
+        <Image width={24} height={24} src={"/images/whatsapp-icon.svg"} alt="" />
+        <Image width={24} height={24} src={"/images/map-icon.svg"} alt="" />
+      </div>
     </header>
   )
 }
