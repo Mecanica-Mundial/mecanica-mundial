409d2d4bda0e979e080795e2cf86feee8fe2bf8c
Thiago Morais <118579263+thiagomorais16@users.noreply.github.com>
2023-08-06 20:56:08 -0300

    create hero

--- src/components/Molecule/Hero/Hero.tsx
+++ src/components/Molecule/Hero/Hero.tsx
@@ -3,21 +3,19 @@ import './Hero.css';
 
 export default function Hero() {
   return (
-    <section>
-        <div id="esquerda">
-            <Image src="/name.jpg" alt="" />
-            <div>
-            Em injeção eletrônica, limpezas de bicos e manutenção geral em veículos leves
-            </div>
-            <div>
-                <button>contato</button>
-                <button>serviços</button>
-            </div>
-
-        </div>
-        <div id="direita">
-            <Image src="/carros.jpg" alt="3 carros"/>
-        </div>
+      <section id='section'>
+        <div id='esquerda'>
+      <h1 id='h1' className="text-4xl font-bold mb-4">CENTRO ESPECIALIZADO</h1>
+      <p id='p' className="text-lg mb-6">Em injeção eletrônica, limpezas de bicos e manutenção geral em veículos leves</p>
+      <div id='button' className="flex flex-wrap items-end mb-8">
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mr-4">Contato</button>
+        <button className="bg-white hover:bg-white-600 text-black font-bold py-2 px-4 rounded">Serviços</button>
+      </div>
+      <div id='direita' >
+        <Image src="/images/carross.png" width={400} height={600} alt="3 carros" className="max-w-full " />
+      </div>
+    </div>
     </section>
-  )
-}
+   );
+};
+
--- src/components/Molecule/ProductSession/ProductSession.tsx
+++ src/components/Molecule/ProductSession/ProductSession.tsx
@@ -1,7 +1,60 @@
+import React, { useState } from 'react';
 
-export default function ProductSession() {
+const ProductSession = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <div>ProductSession</div>
-  )
-}
+    <div className="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+      <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600">
+      </div>
+      <div className="p-6">
+        <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+          Tailwind card
+        </h5>
+        <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula.
+        </p>
+      </div>
+      <div className="p-6 pt-0">
+        <button
+          data-ripple-light="true"
+          type="button"
+          className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          onClick={openModal}
+        >
+          Read More
+        </button>
+      </div>
 
+      {modalOpen && (
+        <div
+          id="modal"
+          className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center"
+        >
+          <div className="bg-white rounded-lg p-6">
+            <h2 className="text-xl font-semibold mb-4">Modal Title</h2>
+            <p className="text-sm text-gray-600">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis ligula.
+            </p>
+            <button
+              className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+              onClick={closeModal}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductSession;
