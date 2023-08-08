e34f911b9c63c418a77de106e85bc6a6d5adf243
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2023-08-07 21:18:41 -0300

    chore: add accessibility attrs on footer's elements

--- src/components/Molecule/Footer/Footer.tsx
+++ src/components/Molecule/Footer/Footer.tsx
@@ -17,7 +17,8 @@ export default function Footer() {
           width={159}
           height={256}
           src={"/images/Logo_transparent.png"}
-          alt=""
+          alt="Logo da Mecânica Mundial - traços formando um globo terrestre"
+          aria-label="Logo da Mecânica Mundial"
           className="h-full"
         />
 
@@ -47,8 +48,28 @@ export default function Footer() {
         <div className="flex gap-6 flex-col">
           <div className="flex gap-3 flex-wrap text-white">
             <p className="inline-block w-full font-bold">REDES SOCIAIS</p>
-            <Whatsapp size="24" variant="Bold" className="text-white hover:text-blue-500"/>
-            <Facebook size="24" variant="Bold" className="text-white hover:text-blue-500"/>
+            <Link 
+              href={"https://wa.me/5535991054663"}
+              target="_blank"
+              aria-label="link para whatsapp da Mecânica Mundial"
+            >
+              <Whatsapp 
+                size="24"
+                variant="Bold"
+                className="text-white hover:text-blue-500"
+              />
+            </Link>
+            <Link 
+              href={"https://www.facebook.com/mecanicamundial"}
+              target="_blank"
+              aria-label="link para a página no facebook da Mecânica Mundial"
+            >
+              <Facebook 
+                size="24"
+                variant="Bold"
+                className="text-white hover:text-blue-500"
+              />
+            </Link>
           </div>
           <div className="flex flex-col gap-3 text-white">
             <p>(35) 3422-1540 (Mecânica)</p>
