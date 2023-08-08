3144521e8eb08ad9a5c66bc18bac8b6d5d0526e4
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2023-08-07 21:05:14 -0300

    chore: add accessibility attrs on header's elements

--- src/components/Molecule/Header/Header.tsx
+++ src/components/Molecule/Header/Header.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
 import { LinkNav } from "../LinkNav";
 import { Map, Whatsapp } from "iconsax-react";
-
-
+import Link from "next/link";
 
 export default function Header() {
 
@@ -10,7 +9,13 @@ export default function Header() {
     <header
       className="flex justify-around items-center w-full h-20 bg-zinc-700 shadow-amber-200 shadow-sm"
     >
-      <Image width={218} height={50} src={"/images/mundial.svg"} alt="" className="w-56"/>
+      <Image 
+        width={218}
+        height={50}
+        src={"/images/mundial.svg"}
+        alt="logo da mecânica mundial - traços que formam um globo terreste"
+        className="w-56"
+      />
       <nav className="h-fit flex gap-2">
         <LinkNav path="/">
           HOME
@@ -29,8 +34,19 @@ export default function Header() {
         </LinkNav>
       </nav>
       <div className="flex gap-4">
-        <Whatsapp size="24" variant="Bold" className="text-white hover:text-blue-500"/>
-        <Map size="24" className="text-white hover:text-blue-500"/>
+        <Link href={"https://wa.me/5535991054663"} target="_blank" aria-label="Whatsapp">
+          <Whatsapp
+            size="24" 
+            variant="Bold" 
+            className="text-white hover:text-blue-500"
+          />
+        </Link>
+        <Link href={"/contact"} aria-label="Localização da Mecânica Mundial">
+          <Map
+            size="24" 
+            className="text-white hover:text-blue-500"
+          />
+        </Link>
       </div>
     </header>
   )
