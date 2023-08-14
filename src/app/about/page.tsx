08e39197bf84468b1b0cc1ec358b5fcd5783d8cd
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-08-14 13:14:57 -0300

    feat: base structure created

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,14 @@
-export default function About () {
+import Image from "next/image"
+import Link from "next/link"
+import mecanica from '/images/C. Automotivo Mundial2.jpg'
 
+export default function About () {
     return (
         <>
-            <h1>Sobre a Empresa</h1>
-            <p>
-                Bem-vindo à nossa oficina com uma história de mais de 30 anos! Fundada em 1987 pelo Toninho, um apaixonado por automóveis que veio de São Paulo aos 14 anos e encontrou em Pouso Alegre sua vida profissional e seu lar. Desde então, estamos comprometidos em oferecer serviços de alta qualidade para nossos clientes.
-                Nosso ambiente é familiar e acolhedor, transmitindo a dedicação e o carinho que permeiam nossa trajetória. Localizada nesta mesma região desde 1990, somos referência na cidade quando se trata de cuidar dos veículos dos nossos clientes.
-                Administramos nossa oficina com muito orgulho e dedicação, contando com uma equipe de mecânicos capacitados e responsáveis, que se dedicam em fornecer um serviço excepcional. Valorizamos a confiança e a satisfação dos nossos clientes, buscando sempre superar suas expectativas.
-            </p>
+            <section className="w-full h-[40rem] bg-whiteMundial flex pr-6 pl-6">
+                <div className=" bg-stone-500 w-[60%] h-[40rem]">Aqui vai as infos</div>
+                <div className=" w-[40%] h-[40rem] bg-stone-700">Aqui vai a img</div>
+            </section>
         </>
     )
 }
