190c13585b6bb4f788dafb934335964bd5a06a95
RaphaelMarquesMartorella <raphael.martorella@hotmail.com>
2023-08-25 20:48:52 -0300

    fixing images

--- src/components/Molecule/BrandsRoulette/BrandsRoulette.tsx
+++ src/components/Molecule/BrandsRoulette/BrandsRoulette.tsx
@@ -1,68 +1,71 @@
-
 "use client";
-import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 
 export default function BrandsRoulette() {
-  
+
   return (
     <div className="bg-primary h-[150px] overflow-hidden">
       <div className="overflow-hidden whitespace-nowrap py-[50px] px-0 w-[calc(500px*28)]">
         <div className="inline-flex animate-scroll">
-          <img src="/images/logos/volkswagen.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/landrover.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/mercedes.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/jaguar.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/chevrolet.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/porshe.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/ford.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/pegeout.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/honda.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/fiat.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/hyundai.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/subaru.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/toyota.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/lamborghini.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/ferrari.svg" alt=""  className="h-[50px] mx-[30px]"/>
+          <Image width={60} height={60} src="/images/logos/volkswagen.svg" alt="carro volkswagen" className="mx-[30px]" />
+          <Image width={85} height={85} src="/images/logos/landrover.svg" alt="carro landrover" className="mx-[30px]" />
+          <Image width={65} height={65} src="/images/logos/mercedes.svg" alt="carro mercedes" className="mx-[30px]" />
+          <Image width={80} height={80} src="/images/logos/jaguar.svg" alt="carro jaguar" className="mx-[30px]" />
+          <Image width={95} height={95} src="/images/logos/chevrolet.svg" alt="carro chevrolet" className="mx-[30px]" />
+          <Image width={50} height={50} src="/images/logos/porshe.svg" alt="carro porshe" className="mx-[30px]" />
+          <Image width={90} height={90} src="/images/logos/ford.svg" alt="carro ford" className="mx-[30px]" />
+          <Image width={55} height={55} src="/images/logos/pegeout.svg" alt="carro pegeout" className="mx-[30px]" />
+          <Image width={60} height={60} src="/images/logos/honda.svg" alt="carro honda" className="mx-[30px]" />
+          <Image width={60} height={60} src="/images/logos/fiat.svg" alt="carro fiat" className="mx-[30px]" />
+          <Image width={85} height={85} src="/images/logos/hyundai.svg" alt="carro hyundai" className="mx-[30px]" />
+          <Image width={70} height={70} src="/images/logos/subaru.svg" alt="carro subaru" className="mx-[30px]" />
+          <Image width={75} height={75} src="/images/logos/toyota.svg" alt="carro toyota" className="mx-[30px]" />
+          <Image width={55} height={55} src="/images/logos/lamborghini.svg" alt="carro lamborghini" className="mx-[30px]" />
+          <Image width={120} height={120} src="/images/logos/ferrari.svg" alt="carro ferrari" className="mx-[30px]" />
+
         </div>
+
         <div className="inline-flex animate-scroll">
-          <img src="/images/logos/volkswagen.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/landrover.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/mercedes.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/jaguar.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/chevrolet.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/porshe.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/ford.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/pegeout.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/honda.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/fiat.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/hyundai.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/subaru.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/toyota.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/lamborghini.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/ferrari.svg" alt=""  className="h-[50px] mx-[30px]"/>
+          <Image width={60} height={60} src="/images/logos/volkswagen.svg" alt="carro volkswagen" className="mx-[30px]" />
+          <Image width={85} height={85} src="/images/logos/landrover.svg" alt="carro landrover" className="mx-[30px]" />
+          <Image width={65} height={65} src="/images/logos/mercedes.svg" alt="carro mercedes" className="mx-[30px]" />
+          <Image width={80} height={80} src="/images/logos/jaguar.svg" alt="carro jaguar" className="mx-[30px]" />
+          <Image width={95} height={95} src="/images/logos/chevrolet.svg" alt="carro chevrolet" className="mx-[30px]" />
+          <Image width={50} height={50} src="/images/logos/porshe.svg" alt="carro porshe" className="mx-[30px]" />
+          <Image width={90} height={90} src="/images/logos/ford.svg" alt="carro ford" className="mx-[30px]" />
+          <Image width={55} height={55} src="/images/logos/pegeout.svg" alt="carro pegeout" className="mx-[30px]" />
+          <Image width={60} height={60} src="/images/logos/honda.svg" alt="carro honda" className="mx-[30px]" />
+          <Image width={60} height={60} src="/images/logos/fiat.svg" alt="carro fiat" className="mx-[30px]" />
+          <Image width={85} height={85} src="/images/logos/hyundai.svg" alt="carro hyundai" className="mx-[30px]" />
+          <Image width={70} height={70} src="/images/logos/subaru.svg" alt="carro subaru" className="mx-[30px]" />
+          <Image width={75} height={75} src="/images/logos/toyota.svg" alt="carro toyota" className="mx-[30px]" />
+          <Image width={55} height={55} src="/images/logos/lamborghini.svg" alt="carro lamborghini" className="mx-[30px]" />
+          <Image width={120} height={120} src="/images/logos/ferrari.svg" alt="carro ferrari" className="mx-[30px]" />
+
         </div>
+
         <div className="inline-flex animate-scroll">
-          <img src="/images/logos/volkswagen.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/landrover.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/mercedes.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/jaguar.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/chevrolet.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/porshe.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/ford.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/pegeout.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/honda.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/fiat.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/hyundai.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/subaru.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/toyota.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/lamborghini.svg" alt=""  className="h-[50px] mx-[30px]"/>
-          <img src="/images/logos/ferrari.svg" alt=""  className="h-[50px] mx-[30px]"/>
+          <Image width={60} height={60} src="/images/logos/volkswagen.svg" alt="carro volkswagen" className="mx-[30px]" />
+          <Image width={85} height={85} src="/images/logos/landrover.svg" alt="carro landrover" className="mx-[30px]" />
+          <Image width={65} height={65} src="/images/logos/mercedes.svg" alt="carro mercedes" className="mx-[30px]" />
+          <Image width={80} height={80} src="/images/logos/jaguar.svg" alt="carro jaguar" className="mx-[30px]" />
+          <Image width={95} height={95} src="/images/logos/chevrolet.svg" alt="carro chevrolet" className="mx-[30px]" />
+          <Image width={50} height={50} src="/images/logos/porshe.svg" alt="carro porshe" className="mx-[30px]" />
+          <Image width={90} height={90} src="/images/logos/ford.svg" alt="carro ford" className="mx-[30px]" />
+          <Image width={55} height={55} src="/images/logos/pegeout.svg" alt="carro pegeout" className="mx-[30px]" />
+          <Image width={60} height={60} src="/images/logos/honda.svg" alt="carro honda" className="mx-[30px]" />
+          <Image width={60} height={60} src="/images/logos/fiat.svg" alt="carro fiat" className="mx-[30px]" />
+          <Image width={85} height={85} src="/images/logos/hyundai.svg" alt="carro hyundai" className="mx-[30px]" />
+          <Image width={70} height={70} src="/images/logos/subaru.svg" alt="carro subaru" className="mx-[30px]" />
+          <Image width={75} height={75} src="/images/logos/toyota.svg" alt="carro toyota" className="mx-[30px]" />
+          <Image width={55} height={55} src="/images/logos/lamborghini.svg" alt="carro lamborghini" className="mx-[30px]" />
+          <Image width={120} height={120} src="/images/logos/ferrari.svg" alt="carro ferrari" className="mx-[30px]" />
+
         </div>
       </div>
     </div>
-   );
+  );
 }
 
 
