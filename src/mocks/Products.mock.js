9513bd64dbf5435f1470a3c4c81ba81df828bc6e
Luana <santos.luanabeatriz1995@gmail.com>
2023-08-27 19:14:15 +0100

    Fixed layout of product cards and product carousel

---
+++ src/mocks/Products.mock.js
@@ -0,0 +1,58 @@
+export const PRODUCTS_MOCK = [
+    {
+        id: 1,
+        image: '/images/products/baterias.svg',
+        title: 'BATERIAS',
+        description: 'Baterias de qualidade são essenciais para fornecer energia confiável ao veículo. Garanta o melhor desempenho e durabilidade.',
+        url: 'https://wa.me/5535991054663?text=Olá%20Mecânica%20Mundial,%20venho%20através%20do%20site%20de%20vocês,%20poderiam%20me%20informar%20a%20respeito%20do%20produto%20BATERIAS?'
+    },
+    {
+        id: 2,
+        image: '/images/products/pneu_pretinho.svg',
+        title: 'PNEU PRETINHO',
+        description: 'Produto de cuidado automotivo que proporciona brilho e proteção, realçando a aparência dos pneus e prolongando sua vida útil.',
+        url: 'https://wa.me/5535991054663?text=Olá%20Mecânica%20Mundial,%20venho%20através%20do%20site%20de%20vocês,%20poderiam%20me%20informar%20a%20respeito%20do%20produto%20PNEU%20PRETINHO?'
+    },
+    {
+        id: 3,
+        image: '/images/products/som_automativo.svg',
+        title: 'SOM AUTOMOTIVO',
+        description: 'Entretenimento e conforto aos ocupantes, transformando cada viagem em uma experiência musical personalizada.',
+        url: 'https://wa.me/5535991054663?text=Olá%20Mecânica%20Mundial,%20venho%20através%20do%20site%20de%20vocês,%20poderiam%20me%20informar%20a%20respeito%20do%20produto%20SOM%20AUTOMOTIVO?'
+    },
+    {
+        id: 4,
+        image: '/images/products/baterias.svg',
+        title: 'BATERIAS',
+        description: 'Baterias de qualidade são essenciais para fornecer energia confiável ao veículo. Garanta o melhor desempenho e durabilidade.',
+        url: 'https://wa.me/5535991054663?text=Olá%20Mecânica%20Mundial,%20venho%20através%20do%20site%20de%20vocês,%20poderiam%20me%20informar%20a%20respeito%20do%20produto%20BATERIAS?'
+    },
+    {
+        id: 5,
+        image: '/images/products/pneu_pretinho.svg',
+        title: 'PNEU PRETINHO',
+        description: 'Produto de cuidado automotivo que proporciona brilho e proteção, realçando a aparência dos pneus e prolongando sua vida útil.',
+        url: 'https://wa.me/5535991054663?text=Olá%20Mecânica%20Mundial,%20venho%20através%20do%20site%20de%20vocês,%20poderiam%20me%20informar%20a%20respeito%20do%20produto%20PNEU%20PRETINHO?'
+    },
+    {
+        id: 6,
+        image: '/images/products/som_automativo.svg',
+        title: 'SOM AUTOMOTIVO',
+        description: 'Entretenimento e conforto aos ocupantes, transformando cada viagem em uma experiência musical personalizada.',
+        url: 'https://wa.me/5535991054663?text=Olá%20Mecânica%20Mundial,%20venho%20através%20do%20site%20de%20vocês,%20poderiam%20me%20informar%20a%20respeito%20do%20produto%20SOM%20AUTOMOTIVO?'
+    },
+    {
+        id: 7,
+        image: '/images/products/pneu_pretinho.svg',
+        title: 'PNEU PRETINHO',
+        description: 'Produto de cuidado automotivo que proporciona brilho e proteção, realçando a aparência dos pneus e prolongando sua vida útil.',
+        url: 'https://wa.me/5535991054663?text=Olá%20Mecânica%20Mundial,%20venho%20através%20do%20site%20de%20vocês,%20poderiam%20me%20informar%20a%20respeito%20do%20produto%20PNEU%20PRETINHO?'
+    },
+    {
+        id: 8,
+        image: '/images/products/som_automativo.svg',
+        title: 'SOM AUTOMOTIVO',
+        description: 'Entretenimento e conforto aos ocupantes, transformando cada viagem em uma experiência musical personalizada.',
+        url: 'https://wa.me/5535991054663?text=Olá%20Mecânica%20Mundial,%20venho%20através%20do%20site%20de%20vocês,%20poderiam%20me%20informar%20a%20respeito%20do%20produto%20SOM%20AUTOMOTIVO?'
+    },
+];
