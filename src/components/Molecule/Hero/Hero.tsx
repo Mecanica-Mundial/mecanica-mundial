fc9eef050f7626db63a9257ebaa8a607f3f3bf73
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-08-29 11:24:54 -0300

    fix: Alignment adjustments made

--- src/components/Molecule/Hero/Hero.tsx
+++ src/components/Molecule/Hero/Hero.tsx
@@ -10,14 +10,14 @@ export default function Hero() {
     <section className='bg-blackMundial h-[90vh] w-full pl-20 pt-6 flex flex-col items-center justify-end'>
       <div className='w-full relative h-[27rem]'>
         <div className='flex flex-col w-full gap-6 items-start justify-center'>
-          <h1 className='font-wall font-bold uppercase text-7xl w-80 text-primary'>Centro Especializado</h1>
+          <h1 className='font-wall font-bold uppercase text-8xl w-80 text-primary'>Centro Especializado</h1>
           <p className='w-[25rem] text-lg'>Em injeção eletrônica, limpezas de bicos e manutenção geral em veículos leves</p>
           <div className='flex w-full gap-6 items-center justify-start'>
             <button className='w-28 h-10 shadow-button font-bold bg-primary text-whiteMundial rounded-xl'>CONTATO</button>
             <button className='w-28 h-10 shadow-button font-bold bg-whiteMundial text-blackMundial rounded-xl'>SERVIÇOS</button>
           </div>
         </div>
-        <Image className='absolute right-0 top-0' src={HeroCar} alt='Carros' aria-label='Carros' width={840} height={360}/>
+        <Image className='absolute right-0 bottom-6' src={HeroCar} alt='Carros' aria-label='Carros' width={840} height={360}/>
     </div>
     <div className='w-full h-14 flex flex-col items-center justify-start gap-2'>
       <p className='text-primary text-xs font-bold'>Role para baixo</p>
